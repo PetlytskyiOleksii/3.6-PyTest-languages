f6d19024b21285d3c3c2945f2923bceb667e8b3f
Oleksii Petlytskyi <aleksey.petlitskiy@gmail.com>
2024-01-21 19:07:16 +0200

    assert add_button present for en/es languages

--- conftest.py
+++ conftest.py
@@ -1,1 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import pytest
+
+
+def pytest_addoption(parser):  # парсер для коммандной строки
+    parser.addoption('--browser_name',
+                     action='store',
+                     default='chrome',  # default=Chrome/Firefox, so then I could skip --browser_name  flag in the commandline
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language',
+                     action='store',
+                     default=None,
+                     help="Choose language: en or es")
+
+
+@pytest.fixture(scope="function")   # scope="class" or “function”, “module”, “session”
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    language = request.config.getoption("language")
+    # Chrome
+    chrome_options = ChromeOptions()
+    # chrome_options.add_argument('--headless')
+    chrome_options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    # Firefox
+    firefox_options = FirefoxOptions()
+    firefox_options.add_argument('--headless')
+    firefox_options.set_preference("intl.accept_languages", language)
+    if browser_name == 'chrome':
+        browser = webdriver.Chrome(options=chrome_options)
+    elif browser_name == 'firefox':
+        browser = webdriver.Firefox(options=firefox_options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    try:
+        yield browser
+    finally:
+        browser.quit()
+
 
--- test_items.py
+++ test_items.py
@@ -1,1 +1,17 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import *
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+add_product_button = By.ID, 'add_to_basket_form'
+
+
+def test_to_check_if_add_to_basket_button_present(browser):
+    browser.get(link)
+    expected_result = WebDriverWait(browser, 10).until(ec.presence_of_element_located(add_product_button)).text
+    print(expected_result)
+    assert expected_result == 'Añadir al carrito'
+
+
 
